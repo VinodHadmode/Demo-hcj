e1a15ca1358a616491f432dcfc5bbf7713727575
VinodHadmode <vinodhadmode@gmail.com>
2023-08-13 01:20:52 +0530

    prac done

--- prep.js
+++ prep.js
@@ -1,17 +1,1 @@
-
-let promise = new Promise((resolve, reject) => {
-
-    setTimeout(() => {
-        const data = "vinod"
-        if (data) {
-            resolve(data)
-
-        } else {
-            reject("Eroorr")
-        }
-    }, 3000);
-})
-
-promise.then((res)=>console.log(res))
-.catch((err)=>console.log(err))
-
+console.log(2+"2"-1);
